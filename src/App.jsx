fdd04c40bf3037c98c03dfba3e9a44f0219ed2fc
giovany Alejandro Alfaro Lopez <giovany.alejandro80@gmail.com>
2025-03-06 21:43:31 -0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,24 @@ function App() {
 
 
   return (
-      <div className='bg-app_lc_background dark:bg-app_ld_background text-app_lc_text dark:text-app_ld_text flex flex-row w-screen h-screen justify-center '>
-        <nav className=' w-[100px] h-fit mt-[10vh] rounded-sm shadow mr-2 z-10 flex flex-col px-2 bg-app_lc_secondary dark:bg-app_ld_secondary'><Nav /></nav>
+      <div className=' bg-app_lc_background 
+      dark:bg-app_ld_background 
+      text-app_lc_text 
+      dark:text-app_ld_text flex 
+      md:flex-row flex-col 
+      w-screen h-fit md:h-screen
+      justify-center '>
+        <nav className='cursor-pointerr w-fit 
+        
+          flex-wrap  
+          md:w-[100px] 
+          h-fit md:mt-[10vh] 
+          rounded-sm shadow 
+          md:mr-2 z-20 flex flex-row md:flex-col 
+          md:px-2
+          bg-app_lc_secondary dark:bg-app_ld_secondary gap-2'>
+            <Nav />
+        </nav>
         <header className='shadown w-[360px] h-[82vh]  mt-[9vh]  rounded-sm z-10 
        shadow-[-5px_-5px_rgba(126,_150,_147,_0.4),_-10px_-10px_rgba(126,_150,_147,_0.3),_-15px_-15px_rgba(126,_150,_147,_0.2)] 
        flex flex-col justify-between
@@ -31,13 +47,21 @@ function App() {
                   <Language/>
                 </div>
             </div>
-            <div className=' flex flex-row z-50' >
+            <div className=' flex flex-row z-50 ' >
               <Header />
             </div>
         </header>
         
-        <section className=' overflow-auto p-4 w-[640px] h-[80vh]  mt-[10vh]  rounded-sm shadow rounded-l-none  bg-app_lc_secondary dark:bg-app_ld_secondary'>
+        <section className=' 
+        md:overflow-auto md:w-[640px] md:h-[80vh]  mt-[10vh] 
+         rounded-sm shadow rounded-l-none 
+         bg-app_lc_secondary  dark:bg-app_ld_secondary
+         '>
+          <div className='border  mx-4 p-4 shadow-[-15px_-0px_rgba(126,_150,_147,_0.4)] 
+' >
+
           {useRoutes(MyRoute)}
+          </div>
 
         </section>
        </div>
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -4,9 +4,9 @@ import { dataNavigation,  } from '../Data';
 import contextLanguage from '../context/Language';
 
 const Button=({title,icon,to,last})=>{
-  return(<Link to={to} className={`flex flex-col border border-x-0 border-t-0 items-center ${last&&"border-none"}  p-2`}>
-    <label>{icon}</label>
-    <label className='align-middle text-center'>{title}</label>
+  return(<Link to={to} className={`flex flex-col w-full  border border-x-0 border-t-0 items-center ${last&&"border-none"}  p-2`}>
+    <span>{icon}</span>
+    <span className='align-middle text-center'>{title}</span>
   </Link>)
 }
 
@@ -15,11 +15,11 @@ const Nav = () => {
   const {getLanguage}= useContext(contextLanguage)
 
   return (
-    <div className='flex flex-col'>
+    <>
       {
         dataNavigation[getLanguage].map((e,i)=><Button {...e} key={i} last={navigation.length-1===i} />)
       }
-    </div>
+      </>
   )
 }
 
